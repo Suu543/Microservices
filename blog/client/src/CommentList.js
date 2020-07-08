7bae68dd7854e72751f8fe382f5fbe738d553b47
YONGSU JEONG <53428342+jos50275266@users.noreply.github.com>
2020-07-08 20:13:10 +0900

    Microservice #14
    - Handling Moderation
    - Updating Comment Content
    - A Quick Test
    - Rendering Comments by Status
    - Dealing with Missing Events
    - Implementing Event Sync
    - Event Syncing in Action

--- blog/client/src/CommentList.js
+++ blog/client/src/CommentList.js
@@ -1,3 +1,30 @@
+import React, { useState, useEffect } from "react";
+
+const CommentList = ({ comments }) => {
+  const renderedComments = () =>
+    comments.map((comment) => {
+      let content;
+
+      if (comment.status === "approved") {
+        content = comment.content;
+      }
+
+      if (comment.status === "pending") {
+        content = "This comment is awaiting moderation";
+      }
+
+      if (comment.status === "rejected") {
+        content = "This comment has been rejected";
+      }
+
+      return <li key={comment.id}>{content}</li>;
+    });
+
+  return <ul>{renderedComments()}</ul>;
+};
+
+export default CommentList;
+
 // import React, { useState, useEffect } from "react";
 // import axios from "axios";
 
@@ -23,16 +50,3 @@
 
 //   return <ul>{renderedComments()}</ul>;
 // };
-
-import React, { useState, useEffect } from "react";
-
-const CommentList = ({ comments }) => {
-  const renderedComments = () =>
-    comments.map((comment) => {
-      return <li key={comment.id}>{comment.content}</li>;
-    });
-
-  return <ul>{renderedComments()}</ul>;
-};
-
-export default CommentList;
